bbb07df3bd17de067dfb5f9b5f97dace5c1f6660
zaray531 <zaraym531@gmail.com>
2026-06-07 19:04:24 -0500

    fix: escuchar en 0.0.0.0

--- src/index.js
+++ src/index.js
@@ -27,7 +27,8 @@ app.get('/', (req, res) => {
 });
 
 // Puerto
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`✅ Servidor corriendo en http://localhost:${PORT}`);
 });
